48ddbd923c5fee0f5f095ea6040fd5fb2b51df13
HP-PC <maristaiu@yahoo.com>
2019-05-03 04:42:31 -0700

    Problema8

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	double a = 6;
+	double b = 2;
+
+        System.out.println("a ^ b = " + Math.pow(a,b));
+    }
+}
